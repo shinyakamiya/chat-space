b9bcf12e2c00300b9f91ee45e4a7ccb5069fc756
shinyakamiya <shinya.600708.kouichi@gmail.com>
2020-05-28 11:23:33 +0900

    chat-space message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
   def index
+    @groups = current_user.groups
   end
   
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def create
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
